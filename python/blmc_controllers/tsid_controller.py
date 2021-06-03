a949717f4488c08e6634119434b05df6596d8017
Avadesh Meduri <avadesh02@gmail.com>
2021-06-03 13:13:22 -0400

    debugging tsid ori

--- python/blmc_controllers/tsid_controller.py
+++ python/blmc_controllers/tsid_controller.py
@@ -16,15 +16,15 @@ class TSID_controller():
             robot : robot object returned by pinocchio wrapper
             eff_arr : end effector name arr
         """
-        self.kp_com = 2.0
-        self.kp_contact = 1.0
-        self.kp_posture = 50.0
-        self.kp_orientation = 100.0
+        self.kp_com = 5.0
+        self.kp_contact = 5.0
+        self.kp_posture = 0.0
+        self.kp_orientation = 0.0
 
-        self.w_com = 1e-5
+        self.w_com = 1.0
         self.w_posture = 1.0e-6
-        self.w_force = 1e1
-        self.w_orientation = 1.0
+        self.w_force = 5e-2
+        self.w_orientation = 1e-6
 
         self.contact_transition = 0.1
 
@@ -48,17 +48,16 @@ class TSID_controller():
         self.mu = mu
 
         # Add Contact Tasks (setup as equality constraints)
-        # contactNormal = np.array([0., 0., 1.])
-        # self.contact_arr = len(self.eff_arr)*[None]
-        # for i, name in enumerate(self.eff_arr):
-        #     print(name)
-        #     self.contact_arr[i] = tsid.ContactPoint(name, self.tsid_robot, name, contactNormal, self.mu, 0.01, 25)
-        #     self.contact_arr[i].setKp(self.kp_contact * np.ones(3))
-        #     self.contact_arr[i].setKd(.03 * np.ones(3))
-            #cnt_ref = self.tsid_robot.framePosition(self.inv_dyn_data, self.tsid_robot.model().getFrameId(name))
-            #self.contact_arr[i].setReference(cnt_ref)
-            # self.contact_arr[i].useLocalFrame(False)
-            #self.invdyn.addRigidContact(self.contact_arr[i], self.w_force)
+        contactNormal = np.array([0., 0., 1.])
+        self.contact_arr = len(self.eff_arr)*[None]
+        for i, name in enumerate(self.eff_arr):
+            self.contact_arr[i] = tsid.ContactPoint(name, self.tsid_robot, name, contactNormal, self.mu, 0.01, 25)
+            self.contact_arr[i].setKp(self.kp_contact * np.ones(3))
+            self.contact_arr[i].setKd(.03 * np.ones(3))
+            cnt_ref = self.tsid_robot.framePosition(self.inv_dyn_data, self.tsid_robot.model().getFrameId(name))
+            self.contact_arr[i].setReference(cnt_ref)
+            self.contact_arr[i].useLocalFrame(False)
+            self.invdyn.addRigidContact(self.contact_arr[i], self.w_force)
 
         # Add posture tasks (in the cost function)
         self.postureTask = tsid.TaskJointPosture("posture_task", self.tsid_robot)
@@ -67,18 +66,31 @@ class TSID_controller():
         self.invdyn.addMotionTask(self.postureTask, self.w_posture, 1, 0.0)
 
         #Add CoM task (in the cost function)
-        # self.comTask = tsid.TaskComEquality("task-com", self.tsid_robot)
-        # self.comTask.setKp(self.kp_com * np.ones(6))
-        # self.comTask.setKd(.01 * np.ones(6))
-        # self.invdyn.addMotionTask(self.comTask, self.w_com, 1, 0.0)
+        self.comTask = tsid.TaskComEquality("task-com", self.tsid_robot)
+        self.comTask.setKp(self.kp_com * np.ones(6))
+        self.comTask.setKd(.01 * np.ones(6))
+        self.invdyn.addMotionTask(self.comTask, self.w_com, 1, 0.0)
+
+        #Add orientation task (in the cost function)
+        self.oriTask = tsid.TaskSE3Equality("orientation", self.tsid_robot, "base_link")
+        self.oriTask.setKp(self.kp_orientation * np.ones(6))
+        self.oriTask.setKd(2.0 * np.sqrt(self.kp_orientation) * np.ones(6))
+        mask = np.ones(6)
+        mask[:3] = 0
+        self.oriTask.setMask(mask)
+        self.invdyn.addMotionTask(self.oriTask, self.w_orientation, 1, 0.0)
 
         #Setup trajectories (des_q, des_v, des_a)
-        # self.com_ref = self.tsid_robot.com(self.inv_dyn_data)
-        # self.trajCom = tsid.TrajectoryEuclidianConstant("trajectory_com", self.com_ref)
-        # self.com_reference = self.trajCom.computeNext()
+        self.com_ref = self.tsid_robot.com(self.inv_dyn_data)
+        self.trajCom = tsid.TrajectoryEuclidianConstant("trajectory_com", self.com_ref)
+        self.com_reference = self.trajCom.computeNext()
 
         self.trajPosture = tsid.TrajectoryEuclidianConstant("trajectory_posture", q0[7:])
         self.traj_reference = self.trajPosture.computeNext()
+
+        self.ori_ref = self.tsid_robot.position(self.inv_dyn_data, self.tsid_robot.model().getJointId("base_link"))
+        self.oriTraj = tsid.TrajectorySE3Constant("trajectory_orientation", self.ori_ref)
+        self.ori_reference = self.oriTraj.computeNext()
 
         self.qp_solver.resize(self.invdyn.nVar, self.invdyn.nEq, self.invdyn.nIn)
 
@@ -88,8 +100,8 @@ class TSID_controller():
         self.kp_posture = kp_posture
         self.kp_orientation = kp_orientation
 
-        # self.comTask.setKp(self.kp_com * np.ones(3))
-        # self.comTask.setKd(2.0 * np.sqrt(self.kp_com) * np.ones(3))
+        self.comTask.setKp(self.kp_com * np.ones(3))
+        self.comTask.setKd(2.0 * np.sqrt(self.kp_com) * np.ones(3))
 
         self.postureTask.setKp(self.kp_posture * np.ones(self.tsid_robot.nv-6))
         self.postureTask.setKd(2.0 * np.sqrt(self.kp_posture) * np.ones(3))
@@ -110,49 +122,27 @@ class TSID_controller():
 
         HQPData = self.invdyn.computeProblemData(t, q, v)
         
-        #Update contacts of inverse dynamics array (i.e. which feet are and are not in contact):
-        #TODO: make this list searchable via names rather than assuming the correct order
-        # for i, name in enumerate(self.eff_arr):
-        #     if self.curr_cnt_array[i] != des_cnt_array[i]:
-        #         self.curr_cnt_array[i] = des_cnt_array[i].copy()
-        #         if des_cnt_array[i] == 0:
-        #             #self.invdyn.removeRigidContact(self.contact_arr[i].name, self.contact_transition)
-        #             print("should remove contact")
-        #             # print(t)
-        #             # print(name)
-        #             # time.sleep(3.0)
-        #         else:
-        #             # time.sleep(0.25)
-        #             # print("Adding contact")
-        #             # print(name)
-        #             # print(t)
-        #             self.inv_dyn_data = self.invdyn.data()
-        #             self.tsid_robot.computeAllTerms(self.inv_dyn_data, q, v)
-        #             cnt_ref = self.tsid_robot.framePosition(self.inv_dyn_data, self.tsid_robot.model().getFrameId(name))
-        #             #self.contact_arr[i].setReference(cnt_ref)
-        #             #self.contact_arr[i].setForceReference(feed_fwd_forces[i*3:i*3 + 3])
-        #             self.invdyn.addRigidContact(self.contact_arr[i], self.w_force)
-
         #Set desired references
-        # self.com_reference.pos(pin.centerOfMass(self.tsid_robot.model(), self.tsid_robot.data(), des_q, des_v))
-        # self.com_reference.vel(des_v[0:3])
-        # self.com_reference.acc(des_a[0:3])
+        self.com_reference.pos(pin.centerOfMass(self.tsid_robot.model(), self.tsid_robot.data(), des_q, des_v))
+        self.com_reference.vel(des_v[0:3])
+        self.com_reference.acc(des_a[0:3])
 
-        # self.traj_reference.pos(des_q)
-        # self.traj_reference.vel(des_v)
+        self.traj_reference.pos(des_q)
+        self.traj_reference.vel(des_v)
         self.traj_reference.acc(des_a)
 
         #Need to update end-effector references here
-        # self.comTask.setReference(self.com_reference)
+        self.comTask.setReference(self.com_reference)
         self.postureTask.setReference(self.traj_reference)
+        self.oriTask.setReference(self.ori_reference)
         # self.postureTask.compute(t, q, v, self.tsid_robot.data())
 
         #print(des_a[6:])
         #print(self.postureTask.getDesiredAcceleration)
 
-        # for i in range(len(self.eff_arr)):
-        #     if des_cnt_array[i] == 1:
-        #         self.contact_arr[i].setForceReference(feed_fwd_forces[i*3:i*3 + 3])
+        for i in range(len(self.eff_arr)):
+            if des_cnt_array[i] == 1:
+                self.contact_arr[i].setForceReference(feed_fwd_forces[i*3:i*3 + 3])
 
         #Solve
         #print(feed_fwd_forces)
@@ -169,7 +159,6 @@ class TSID_controller():
         #     print("[%d] QP problem could not be solved! Error code:"%(i), sol.status)
 
         tau = self.invdyn.getActuatorForces(sol)
-        print(tau.shape)
-        # tau_gain = -1.0*(np.subtract(q[7:], des_q[7:].T)) - 0.09*(np.subtract(v[6:], des_v[6:].T))
-        # tau += tau_gain
+        tau_gain = -1.5*(np.subtract(q[7:], des_q[7:].T)) - 0.1*(np.subtract(v[6:], des_v[6:].T))
+        tau += tau_gain
         return tau
